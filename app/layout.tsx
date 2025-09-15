344bec7f573cc72eeacbc7f697ed9cb2aec3d60f
reelmza <jessemoses71@gmail.com>
2025-09-15 16:12:01 +0100

    Updated sizing and spacing on mobile info screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
       <body className={`${inter.variable} antialiased`}>
         {/* Info text for smaller screens */}
         <div className="lg:hidden h-full w-full bg-neutral-50 flex flex-col items-center justify-center">
-          <Info size={24} className="text-accent-dim" />
-          <div className="px-10 py-5 text-center text-accent-dim font-sans font">
+          <Info size={32} className="text-accent-dim" />
+          <div className="px-5 py-5 text-center text-accent-dim font-sans font">
             This app is in development, <br /> please use a bigger device to
             preview.
           </div>
